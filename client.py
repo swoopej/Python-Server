3b9adce8239acfdc62aaf0fa811b53f0cf36001f
swoopej <swoopej@gmail.com>
2012-10-24 12:30:58 -0400

    refactored server and client

--- client.py
+++ client.py
@@ -1,9 +1,7 @@
-#!//usr/bin/env python
-
 from socket import *
 
 HOST = 'localhost'
-PORT = 21567
+PORT = 21555
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 
--- http_test.py
+++ http_test.py
@@ -1,33 +1,49 @@
-from socket import *
+import socket
 from time import ctime
 import os
 
 class Server:
 	#class to make a simple http server
-	def __init__(self, port = 21567):
+	def __init__(self, port = 21555):
 		self.host = ''
 		self.port = port
 		self.bufsiz = 1024
 
-	def run_server(self, port = 21567):
-		self.socket = socket(AF_INET, SOCK_STREAM)
-		self.socket.bind((self.host, self.port))
+	def run_server(self, port = 21555):
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.s.bind((self.host, self.port))
+		self.s.listen(5)
 
-		while True:
-			self.socket.listen(5)
-			print 'waiting for connection on port %s...' % port
-			self.clisock, addr = self.socket.accept()
-			os.fork()
-			print 'connected from : ', addr
-			while True:
-				data = self.clisock.recv(1024)
-				if not data:
-					self.clisock.close()
-					break
-				self.clisock.send('You sent the following data: %s' % (data))
+		for x in range(5):
+			pid = os.fork()
 
-		new_server.socket.close()
+			if pid == 0:
+				print 'waiting for connection on port %s...' % port
+				
+				try:
+					self.clisock, addr = self.s.accept()
+					print 'connected from : ', addr
+
+					while True:
+						data = self.clisock.recv(1024)
+
+						if not data:
+							self.clisock.close()
+							break
+
+						self.clisock.send('You sent the following data: %s' % (data))
+				except KeyboardInterrupt:
+					sys.exit()
+
+		
 
 
 new_server = Server()
 new_server.run_server()
+try:
+	os.waitpid(-1, 0)
+except KeyboardInterrupt:
+	sys.exit()
+new_server.s.close()
+
